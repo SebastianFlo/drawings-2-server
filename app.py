d1d7088bee89435e09857036408913ba13c859d0
Sebastian Florian <rebardesigndk@gmail.com>
2018-05-07 22:04:04 +0200

    feature(app): Add postgress

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import jsonify
 from flask_restful import Api
@@ -12,7 +14,8 @@ from resources.item import Item, Items
 from resources.store import Store, Stores
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'  # this should be a secret
 api = Api(app)
