c90edb757e459fcb91478263a7301dcc5bd63ef4
Sebastian Florian <rebardesigndk@gmail.com>
2018-06-05 09:49:31 +0200

    fix(app): Secret key

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-# app.secret_key = os.environ['DDBPASS']
+app.secret_key = os.environ['DDBPASS']
 api = Api(app)
 
 jwt = JWTManager(app)
